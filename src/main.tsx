f25e88725465c0d69fe8b53df6a190475d0d39c5
Purushottam Khedre <purushottamkhedre123@gmail.com>
2023-09-16 01:37:13 +0530

    finalizing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles.css";
+import { createHashRouter, RouterProvider } from "react-router-dom";
+import routes from "./routes";
+
+const router = createHashRouter(routes);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
